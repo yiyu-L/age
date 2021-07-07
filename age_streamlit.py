50fb0f3a35f002223140d100039231e7f240b1b5
yiyu-L <66548524+yiyu-L@users.noreply.github.com>
2021-07-07 11:17:57 +0800

    Update age_streamlit.py

--- age_streamlit.py
+++ age_streamlit.py
@@ -7,8 +7,11 @@ import streamlit as st
 import base64
 import io
 
-
-@st.cache(persist=False, allow_output_mutation=True, suppress_st_warning=False, show_spinner= True)
+@st.cache(persist=False, allow_output_mutation=True, suppress_st_warning=False, show_spinner= False)
+def load_data():
+    df_input = pd.DataFrame()
+    df_input = pd.read_excel(input, engine = 'openpyxl', sheet_name = 0)
+    return df_input
 
 def age_trans(x):
     if isinstance(x, str):
@@ -39,6 +42,22 @@ def tb_in(x):
     else:
         return 0
 
+def prep_data(df):
+    df['年龄Y'] = df['年龄'].apply(age_trans).astype(float).apply(np.ceil).apply(ge_120)
+    df['tb_标注'] = df['医生审核结果'].apply(tb_in).astype(int)
+    df['tb分值'] = df['AI分值']
+    df = df[['年龄', 'AI分值', '医生审核结果', '年龄Y', 'tb_标注', 'tb分值']]
+    return df
+
+def AI_threshold_set(df):
+    df['tb_AI'] = (df['tb分值'] > AI_threshold*0.01).astype(int)
+    return df
+
+def age_bar(df):
+    df_age = df['年龄Y'].value_counts()
+    fig = px.bar(df_age, labels = {'index':'年龄','value': '数量'}, title = '年龄分布')
+    return fig
+
 def thre_compute(x):
     total = len( x )
     tp = ( ( x['tb_标注'] ==1) & ( x['tb_AI'] == 1 ) ).astype(int).sum()  # 真阳
@@ -51,11 +70,6 @@ def thre_compute(x):
     p_fn_rate = fn / p_num  # 阳性患者假阴率
     return total, p_num, tp, tn, fn, fp, fn_rate, fp_rate, p_fn_rate
 
-def age_bar(df):
-    df_age = df['年龄Y'].value_counts()
-    fig = px.bar(df_age, labels = {'index':'年龄','value': '数量'}, title = '年龄分布')
-    return fig
-
 def result_output(df):
     mat_up = []
     for threshold_age in range(-1, 70):
@@ -67,21 +81,6 @@ def result_output(df):
     mat = np.hstack( (  np.array(mat_up), np.array(mat_low)  ) )
     matr = pd.DataFrame( mat, columns = [['年龄阈值及以上的分析(包含年龄阈值)']*10 + ['年龄阈值及以下的分析(包含年龄阈值)'] * 10, ['年龄阈值', '样本数量', '阳性数量', '真阳', '真阴', '假阴', '假阳','假阴率', '假阳率', '阳性患者假阴率']*2 ] )
     return matr
-
-def load_data():
-    df_input = pd.DataFrame()
-    df_input = pd.read_excel(input, engine = 'openpyxl', sheet_name = 0)
-    return df_input
-
-def prep_data(df):
-    df['年龄Y'] = df['年龄'].apply(age_trans).astype(float).apply(np.ceil).apply(ge_120)
-    df['tb_标注'] = df['医生审核结果'].apply(tb_in).astype(int)
-    df['tb分值'] = df['AI分值']
-    return df
-
-def AI_threshold_set(df):
-    df['tb_AI'] = (df['tb分值'] > AI_threshold*0.01).astype(int)
-    return df
 
 
 if __name__ == "__main__":
@@ -103,28 +102,31 @@ if __name__ == "__main__":
             sample = st.checkbox("从GitHub上下载demo.xlsx")
     try:
         if sample:
-            st.markdown("""[下载链接](https://www.baidu.com/)""")    
+            st.markdown("""[下载链接](https://github.com/yiyu-L/age)""")    
     except:
        if input:
            with st.spinner('加载数据中...'):
                 df = load_data()
     with st.beta_expander('显示原始数据'):
       st.write(df)
-    df1 = prep_data(df)
+    
 
     st.subheader('2.年龄分布图')
-    df1 = pd.DataFrame(df1)
-    radar_chart_fig = age_bar(df1)
-    st.plotly_chart(radar_chart_fig)
+    if not df.empty:
+        df1 = prep_data(df)
+        df1 = pd.DataFrame(df1)
+        radar_chart_fig = age_bar(df1)
+        st.plotly_chart(radar_chart_fig)
 
     st.subheader('3.不同年龄阈值的结果')
     AI_threshold = st.slider("AI阈值设置(%)", 0, 100)
-    df2 = AI_threshold_set(df1)
-    result = result_output(df2)
-    st.write(result)
-    df_export = pd.DataFrame(result)
-    output = io.BytesIO()
-    df_export.to_excel(output, engine = 'openpyxl') 
-    b64 = base64.b64encode(output.getvalue())
-    href = f'<a download = "年龄阈值分析结果.xlsx" href="data:file/xlsx;base64,{b64}">Download XLSX File</a> (**年龄阈值分析结果.xlsx**)'
-    st.markdown(href, unsafe_allow_html=True)
+    if not df.empty:
+        df2 = AI_threshold_set(df1)
+        result = result_output(df2)
+        st.write(result)
+        df_export = pd.DataFrame(result)
+        output = io.BytesIO()
+        df_export.to_excel(output, engine = 'openpyxl') 
+        b64 = base64.b64encode(output.getvalue())
+        href = f'<a download = "年龄阈值分析结果.xlsx" href="data:file/xlsx;base64,{b64}">Download XLSX File</a> (**年龄阈值分析结果.xlsx**)'
+        st.markdown(href, unsafe_allow_html=True)
